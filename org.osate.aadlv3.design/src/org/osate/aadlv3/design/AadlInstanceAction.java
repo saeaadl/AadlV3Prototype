208ff219b6e423bb821e0f80e2a0d22c89c759a7
Peter Feiler <phf@sei.cmu.edu>
2019-03-01 21:05:32 -0500

    Got instance action working flow highlighting works too.

--- org.osate.aadlv3.design/src/org/osate/aadlv3/design/AadlInstanceAction.java
+++ org.osate.aadlv3.design/src/org/osate/aadlv3/design/AadlInstanceAction.java
@@ -5,45 +5,50 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.action.AbstractExternalJavaAction;
+import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DNodeSpec;
+import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DSemanticDiagramSpec;
 import org.osate.av3instance.av3instance.InstanceObject;
 
 public class AadlInstanceAction extends AbstractExternalJavaAction {
 
+	@SuppressWarnings("restriction")
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
-//		for (EObject eo : arg0) {
-//			EObject target = null;
-//			if (eo instanceof DNodeSpec) {
-//				DNodeSpec ds = (DNodeSpec) eo;
-//				target = ds.getTarget();
-//			}
-//			if (eo instanceof DSemanticDiagramSpec) {
-//				DSemanticDiagramSpec ds = (DSemanticDiagramSpec) eo;
-//				target = ds.getTarget();
-//			}
-//			if (target instanceof InstanceObject) {
-//				return true;
-//			}
-//		}
+		for (EObject eo : arg0) {
+			EObject target = null;
+			if (eo instanceof DNodeSpec) {
+				DNodeSpec ds = (DNodeSpec) eo;
+				target = ds.getTarget();
+			}
+			if (eo instanceof DSemanticDiagramSpec) {
+				DSemanticDiagramSpec ds = (DSemanticDiagramSpec) eo;
+				target = ds.getTarget();
+			}
+			if (target instanceof InstanceObject) {
+				return true;
+			}
+		}
 		return false;
 	}
 
+	@SuppressWarnings("restriction")
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
-//		for (EObject eo : arg0) {
-//			EObject target = null;
-//			if (eo instanceof DNodeSpec) {
-//				DNodeSpec ds = (DNodeSpec) eo;
-//				target = ds.getTarget();
-//			}
-//			if (eo instanceof DSemanticDiagramSpec) {
-//				DSemanticDiagramSpec ds = (DSemanticDiagramSpec) eo;
-//				target = ds.getTarget();
-//			}
-//			if (target instanceof InstanceObject) {
-//				perform((InstanceObject) target);
-//			}
-//		}
+		for (EObject eo : arg0) {
+			EObject target = null;
+			if (eo instanceof DNodeSpec) {
+				DNodeSpec ds = (DNodeSpec) eo;
+				target = ds.getTarget();
+			}
+			if (eo instanceof DSemanticDiagramSpec) {
+				DSemanticDiagramSpec ds = (DSemanticDiagramSpec) eo;
+				target = ds.getTarget();
+			}
+			if (target instanceof InstanceObject) {
+				perform((InstanceObject) target);
+			}
+		}
+		return;
 	}
 
 	public void perform(InstanceObject io) {
